099b3dbff6398ffd15f57a708e331f3113924927
Noah Sylwester <nanoabram@comcast.net>
2020-07-01 18:12:47 -0700

    init app, construct splash page Splash.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Splash />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Splash from './components/Splash.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Splash
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  height: 100vh;
+  width: 100vw;
+  background-color: black;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/Splash.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="splash">
+        <img id="mars-img" alt="mars image" src="../assets/mars.png">
+        <h1>
+            MARS EXPLORER
+        </h1>
+        <p>
+            traverse the surface of Mars through images captured by Mars rovers
+        </p>
+        <button>enter</button>
+        <img id="earth-img" alt="earth image" src="../assets/earth.png">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Splash',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#mars-img {
+  width: 10vw;
+  height: 10vw;
+  object-fit: cover;
+}
+#earth-img {
+  width: 60vw;
+  height: 60vw;
+  object-fit: cover;
+  border-radius: 1000vw;
+}
+.splash {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+button {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: black;
+    color: white;
+    border: 0;
+    font-size: 2vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 7vw;
+    height: 7vw;
+    padding: 10px;
+    border-radius: 100px;
+    box-shadow: 0 0 40px white;
+    transition: 0.2s;
+    cursor: pointer;
+}
+button:hover {
+    background-color: white;
+    color: black;
+}
+button:focus {
+    outline: none;
+}
+</style>
